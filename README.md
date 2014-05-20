706645a182a93d8ef76b86c7030940d9e6139960
Wei Gao <jky239@gmail.com>
2014-05-20 10:50:36 +0800

    find dependencies structures, update doc

--- README.md
+++ README.md
@@ -1,6 +1,47 @@
-# Cortex Singlify
+# Cortex Shrinkwrap
 
-Build the package and its dependencies into one file.
+This command will lock down the versions of a package's dependencies and generate a `cortex-shrinkwrap.json` file in your package directory.
+
+## Install 
+
+``` bash
+npm install cortex-shrinkwrap -g
+```
+
+## Usage
+
+``` bash
+cortex shrinkwrap [--dev] [--async|--no-async]
+```
+
+See more info by:
+
+``` bash
+cortex shrinkwrap -h
+```
+
+## API
+
+Also this lib can be used in nodejs.
+
+```javascript
+var shrinkwrap = require('cortex-shrinkwrap');
+
+shrinkwrap(pkg, options, function(err, shrinked) {
+    // console.log(shrinked);
+});
+
+```
+
+### pkg
+
+Pakcage information stored in cortex.json.
+
+### Options
+
+* dev: whehter include `devDependencies`
+* async: whether incldue `asyncDependencies`
+
 
 
 ## License
--- bin/shrinkwrap.js
+++ bin/shrinkwrap.js
@@ -9,44 +9,46 @@ var argv = require('minimist')(process.argv.slice(2));
 
 if (argv.version || argv.v) {
     var pkg = require('../package.json');
-    console.log([pkg.name, 'v' + pkg.version].join(' '));
+    process.stdout.write([pkg.name, 'v' + pkg.version].join(' ') + "\n");
     process.exit(0);
 }
 
 
-var dev = argv.dev;
+if (argv.help || argv.h) {
+    var help = ["Usage: cortex shrinkwrap [--dev] [--async] [options]",
+        "", "Options:", "  -v, --version\t\tprint version",
+        "  -h, --help\tshow help",
+        "  --dev, --no-dev\tshrinkwrap devDependencies or not",
+        "  --async, --no-async\tshrinkwrap asyncDependencies or not, default is true"
+    ];
+    process.stdout.write(help.join("\n"));
+    process.exit(0);
+}
 
 
-var cwd = process.cwd();
+var dev = argv.dev,
+    async = argv.async;
 
-// console.log(built_root, cwd, process.argv);
+if (async === undefined) // async default to true
+    async = true;
 
-var pkg_file = path.join(cwd, 'package.json');
-
-
-var pkg_file = path.join(cwd, 'package.json');
-
-
-fs.exists(pkg_file, function(exists) {
-    if (!exists) {
-        process.stderr.write("Can not find 'package.json' file in current directory\n");
+require('../lib/read-json')(process.cwd(), function(err, pkg) {
+    if (err) {
+        process.stderr.write(err.message);
+        e.stack && process.stderr.write(e.stack + "\n");
         process.exit(1);
     }
 
-    var pkg;
-    try {
-        pkg = require(pkg_file);
-    } catch (e) {
-        process.stderr.write("Error when read package.json:" + e.message + "\n");
-        process.stderr.write(e.stack + "\n");
-        process.exit(1);
-    }
-
-    require('../lib')(pkg, function(err, shrinkwrap) {
+    require('../lib')(pkg, {
+        dev: dev,
+        async: async
+    }, function(err, shrinkwrap) {
         if (err) {
             process.stderr.write(e.message + "\n");
-            process.stderr.write(e.stack + "\n");
+            e.stack && process.stderr.write(e.stack + "\n");
             process.exit(1);
         }
+
+        // console.log(JSON.stringify(shrinkwrap, null, 4));
     });
 });
